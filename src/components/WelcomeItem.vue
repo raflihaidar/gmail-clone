efae508c82d7c3a4caff67dfff26ba5085565dd8
rafli.haidar30@gmail.com <rafli.haidar30@gmail.com>
2023-08-16 21:40:07 +0700

    first commit

---
+++ src/components/WelcomeItem.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="text-4xl bg-red-500 w-screen h-screen">
+    <p>Hallo Semua</p>
+  </div>
+</template>
---
+++ src/views/MainView.vue
@@ -0,0 +1,7 @@
+<template>
+    <WelcomeItem />
+</template>
+
+<script setup>
+import WelcomeItem from '../components/WelcomeItem.vue';
+</script>
